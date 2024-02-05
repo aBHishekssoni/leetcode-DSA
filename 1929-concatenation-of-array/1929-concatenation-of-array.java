f9c008f4557437282afd9215edb782ab6c79508f
aBHishekssoni <116372321+aBHishekssoni@users.noreply.github.com>
2024-02-05 23:11:52 +0530

    Time: 2 ms (15.21%), Space: 42.6 MB (98.97%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,7 +1,12 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
-var getConcatenation = function(nums) {
-    return [...nums,...nums]
-};
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int len = nums.length;
+        int[] ans = new int[2*len];
+        for(int i = 0; i < len; i++){
+            ans[i] = nums[i];
+            ans[i+len] = nums[i];
+        }
+        System.gc();
+        return ans;
+    }
+}
