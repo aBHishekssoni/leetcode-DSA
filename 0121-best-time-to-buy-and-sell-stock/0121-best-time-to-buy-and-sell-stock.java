67e4e57e9ea44578cc2f7e4bb6f343735e11addb
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2024-11-29 07:50:30 +0530

    Time: 1 ms (99.78%), Space: 61.6 MB (50.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,31 +1,28 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // int p = 0,a=0,m=Integer.MAX_VALUE,i=0;
-        // for(i=0;i<prices.length;i++){
-        //     if(m>=prices[i]){
-        //         m = prices[i];
-        //         a=i;
-        //     }
+        // int n = prices.length;
+        // int min = prices[0];
+        // int profit = 0;
+        // for(int i=1;i<n;i++) {
+        //     int temp = prices[i] - min;
+        //     min = Math.min(prices[i],min);
+        //     if(profit < temp)  profit = temp;           
         // }
-        // if(a==prices.length-1) return 0;
-        // for(int j=a+1;j<prices.length;j++){
-        //     if(m<prices[j]&&p<prices[j])
-        //     p= prices[j];
-        //     if(p>m&&j==prices.length-1)
-        //     return (p-m);
-        // }
-        // return 0;
+        // return profit;
 
-    
+
+         int maxProf = 0;
         int n = prices.length;
-        int min = prices[0];
-        int profit = 0;
-        for(int i=1;i<n;i++) {
-            int temp = prices[i] - min;
-            min = Math.min(prices[i],min);
-            if(profit < temp)  profit = temp;           
+        int buyPrice = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            int currPrice = prices[i];
+            if(currPrice > buyPrice){
+                maxProf = Math.max(maxProf, currPrice - buyPrice);
+            }else{
+                buyPrice = currPrice;
+            }
         }
-        return profit;
+        return maxProf;
 	
     }
 }
