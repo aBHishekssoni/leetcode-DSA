08fb9c96f059540e36e17fb5599dea1a0c8d986c
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2025-09-04 16:11:47 +0530

    [228] [Time Beats: 100.00%] [Memory Beats: 96.82%] - LeetPush

--- Summary-Ranges.java
+++ Summary-Ranges.java
@@ -1,27 +1,30 @@
-import java.util.*;
-
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        List<String> result = new ArrayList<>();
-        if (nums.length == 0) return result;
+        if (nums.length == 0) {
+            return new ArrayList<>();
+        }
 
-        int start = nums[0]; // beginning of current range
+        List<String> ranges = new ArrayList<>();
+        int start = nums[0], end = nums[0];
 
         for (int i = 0; i < nums.length; i++) {
-            // if it's the end of the range (last element or next is not consecutive)
-            if (i == nums.length - 1 || nums[i] + 1 != nums[i + 1]) {
-                if (start == nums[i]) {
-                    result.add(String.valueOf(start)); // single element range
-                } else {
-                    result.add(start + "->" + nums[i]); // range
-                }
-                // update start for next range if not last
+            end = nums[i];
+
+            if (i == nums.length - 1 || nums[i + 1] != nums[i] + 1) {
+                ranges.add(generateRange(start, end));
+
                 if (i != nums.length - 1) {
                     start = nums[i + 1];
                 }
             }
         }
+        
+        return ranges;
+    }
 
-        return result;
+    private String generateRange(int start, int end) {
+        return start != end
+            ? new StringBuilder().append(start).append("->").append(end).toString() 
+            : new StringBuilder().append(end).toString();
     }
 }
