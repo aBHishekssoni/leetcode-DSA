1a969232e77afe2bca38edb5e8036ad04c8b435c
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2025-09-04 15:05:08 +0530

    [217] [Time Beats: 36.89%] [Memory Beats: 18.84%] - LeetPush

---
+++ Contains-Duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i:nums){
+            hs.add(i);
+        }
+        if(nums.length==hs.size())
+        return false; 
+        return true;
+    }
+}
