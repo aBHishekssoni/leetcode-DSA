a6c3408548806a6781939ed652d9a1737b2e69d3
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2025-09-05 17:59:01 +0530

    [575] [Time Beats: 50.61%] [Memory Beats: 64.54%] - LeetPush

--- Distribute-Candies.java
+++ Distribute-Candies.java
@@ -1,12 +1,8 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
         HashSet<Integer> hs = new HashSet<Integer>();
-        for(int i:candyType){
+        for(int i:candyType)
             hs.add(i);
-        }
-        if(hs.size()<candyType.length/2){
-            return hs.size();
-        }
-        return candyType.length/2;
+        return Math.min(hs.size(),candyType.length/2);
     }
 }
