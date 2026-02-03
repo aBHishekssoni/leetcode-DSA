9676e800fd0b1af43d208536c8b62a995bb7a3e7
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2026-02-03 14:08:57 +0530

    [231] [Time Beats: 95.61%] [Memory Beats: 50.07%] - LeetPush

--- Power-of-Two.java
+++ Power-of-Two.java
@@ -1,19 +1,8 @@
 1class Solution {
-2    public int reverse(int x) {
-3        int rev = 0;
-4        while (x != 0) {
-5            int pop = x % 10;
-6            x /= 10; 
-7            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE/10 && pop > 7)) {
-8                return 0;
-9            }
-10            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE/10 && pop < -8)) {
-11                return 0; 
-12            }
-13            
-14            rev = rev * 10 + pop;
-15        }
-16        return rev;
-17    }
-18}
-19
+2    public boolean isPowerOfTwo(int n) {
+3        if(n==1) return true;
+4        if(n>0&&n%2==0)
+5        return isPowerOfTwo(n/2);
+6        return false;
+7    }
+8}
