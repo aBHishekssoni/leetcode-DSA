ce67382987bf159e994d3699489cf2bb96be0ce7
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2026-02-05 12:54:54 +0530

    [2859] [Time Beats: 22.39%] [Memory Beats: 23.16%] - LeetPush

---
+++ Sum-of-Values-at-Indices-With-K-Set-Bits.java
@@ -0,0 +1,22 @@
+1class Solution {
+2    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+3        if(k==0) return nums.get(0);
+4        int sum=0;
+5        for(int i=0;i<nums.size();i++){
+6            if(hammingWeight(i)==k){
+7                sum+=nums.get(i);
+8            }
+9        }
+10        return sum;
+11    }
+12        public int hammingWeight(int n) {
+13        if(n==0) return 0;
+14        if(n==1) return 1;
+15        int nums= 0;
+16        while(n>0){
+17            if(n%2!=0) nums++;
+18            n/=2;
+19        }
+20        return nums;
+21    }
+22}
