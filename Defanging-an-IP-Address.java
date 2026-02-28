d7700097e2cdd8b52f25d2c33dec669615cd6332
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2026-02-28 20:06:49 +0530

    [1108] [Time Beats: 100.00%] [Memory Beats: 90.87%] - LeetPush

--- Defanging-an-IP-Address.java
+++ Defanging-an-IP-Address.java
@@ -1,11 +1,12 @@
 1class Solution {
 2    public String defangIPaddr(String address) {
-3        String s = "";
-4        for(int i=0;i<address.length();i++){
-5           if(address.charAt(i) == '.'){
-6                s+= "[.]";
-7            }else s += address.charAt(i);
-8        }
-9        return s;
-10    }
-11}
+3        // String s = "";
+4        // for(int i=0;i<address.length();i++){
+5        //    if(address.charAt(i) == '.'){
+6        //         s+= "[.]";
+7        //     }else s += address.charAt(i);
+8        // }
+9        // return s;
+10        return address.replace(".","[.]");
+11    }
+12}
