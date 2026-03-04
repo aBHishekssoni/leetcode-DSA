b0ddae72f2abe754da369f3e653e722c4454ebde
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2026-03-04 19:44:27 +0530

    [3210] [Time Beats: 32.13%] [Memory Beats: 84.08%] - LeetPush

--- Find-the-Encrypted-String.java
+++ Find-the-Encrypted-String.java
@@ -1,9 +1,8 @@
 1class Solution {
 2    public String getEncryptedString(String s, int k) {
-3        // if(k>=s.length()) return s;
-4        k = k%s.length();
-5        String st = ""+s.substring(k, s.length());;
-6        st = st+s.substring(0, k);
-7        return st;
-8    }
-9}
+3        k = k%s.length();
+4        String st = ""+s.substring(k, s.length());;
+5        st = st+s.substring(0, k);
+6        return st;
+7    }
+8}
