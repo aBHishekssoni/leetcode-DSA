932f7b859774ef7e5cd652b8ecfa2d170febf714
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2026-05-28 10:57:07 +0530

    [292] [Time Beats: 100.00%] [Memory Beats: 52.53%] - LeetPush

---
+++ Nim-Game.java
@@ -0,0 +1,14 @@
+1class Solution {
+2    public boolean canWinNim(int n) {
+3        // if(n<=3) return true;
+4        if(n%4==0) return false;
+5        // int c=0;
+6        // while(n>3){
+7        //     c++;
+8        //     n-=3;
+9        // }
+10        // if(c%2==0) 
+11        return true;
+12        // return false;
+13    }
+14}
