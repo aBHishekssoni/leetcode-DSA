8822c2aae3b89247cd60cb16a97449f0514c5d79
Abhishek Soni <116372321+aBHishekssoni@users.noreply.github.com>
2026-05-28 10:56:38 +0530

    Time: 0 ms (100%), Space: 42 MB (52.53%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        // if(n<=3) return true;
+        if(n%4==0) return false;
+        // int c=0;
+        // while(n>3){
+        //     c++;
+        //     n-=3;
+        // }
+        // if(c%2==0) 
+        return true;
+        // return false;
+    }
+}
